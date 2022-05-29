214f9ca1ad4c255453f5e15bb1a5b25b9da4d155
Daofong12 <peter9236@gmail.com>
2022-05-29 20:28:46 +0800

    add nav

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     var tabTitle = arrayOf("Home","Record","Message")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +23,6 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(tl,pager){
             tab,position->tab.text=tabTitle[position]
         }.attach()
+
     }
 }
